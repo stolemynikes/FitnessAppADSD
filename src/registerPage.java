7735931e1147625e362229e3d090ed2eaec523da
stolemynikes <stolemynikes@gmail.com>
2025-02-13 15:25:20 +0100

    added menu's

--- src/registerPage.java
+++ src/registerPage.java
@@ -4,30 +4,35 @@ public class registerPage {
 
     public static void Register() {
 
+        Scanner scan  = new Scanner(System.in);
+
         System.out.println("Please provide a username and password to register");
 
-            Scanner scan = new Scanner(System.in);
-
         System.out.println("Username:");
-
-            String username = scan.nextLine();
+        String username = scan.nextLine();
 
         System.out.println("Password:");
+        String password = scan.nextLine();
 
-            String password = scan.nextLine();
+        String passwordConfirmation;
 
-        System.out.println("Please confirm your password:");
+        while(true) {
 
-            String passwordConfirmation = scan.nextLine();
+            System.out.println("Please confirm your password");
+            passwordConfirmation = scan.nextLine();
+
+            if(password.equals(passwordConfirmation)) {
+                System.out.println("Registration succesful, welcome: " + username);
+                break;
+            } else {
+                System.out.println("Passwords do not match, please try again:");
+            }
+
+        }
 
         scan.close();
 
-        if(password.equals(passwordConfirmation)) {
-            System.out.println("You have succesfully registerd!");
-        } else {
-            System.out.println("The passwords did not match, please try again.");
-            Register();
-        }
-
     }
+
+
 }
